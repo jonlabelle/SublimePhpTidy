8a5dd3e18084b6c389af612ca52a46a7c3ffa425
Jon LaBelle <jon@tech0.com>
2015-01-14 18:47:22 -0600

    Init commit.

---
+++ README.md
@@ -0,0 +1,27 @@
+# Sublime Text PHP Allman Code Style
+
+A [Sublime Text](http://www.sublimetext.com) plug-in to style PHP code 
+according to [Allman Coding Standards](http://en.wikipedia.org/wiki/Indent_style#Allman_style) (BSD Style).
+
+## Install
+
+Download the [zip](https://github.com/jonlabelle/SublimePhpTidy/archive/master.zip)
+to your Sublime Text 2 or 3 `Packages` directory.
+
+## Usage
+
+- `ctrl/cmd + shift + P` and type `Tidy PHP`.
+
+## Credits
+
+- PHP Tidy script by Magnus Rosenbaum <phptidy@cmr.cx>
+- Original Sublime PHP Tidy [welovewordpress/SublimePhpTidy](https://github.com/welovewordpress/SublimePhpTidy)
+
+**Enhancements from [welovewordpress/SublimePhpTidy](https://github.com/welovewordpress/SublimePhpTidy) include**:
+
+- Sublime Text 2 and 3 compatible.
+- Cross-platform compatible.
+- Merge latest PHP Tidy script updates.
+- Cross-platform support for temp files.
+- Cross platform support for PHP Tidy script.
+- Improved error handling in Sublime Text.
---
+++ php_tidy.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import re
+import subprocess
+import shutil
+import tempfile
+import platform
+import sublime
+import sublime_plugin
+
+PY_VERSION = sys.version_info[0]
+ST_VERSION = sublime.version()[0]
+OS_PLATFORM = platform.system()
+
+PLUGIN_PATH = os.path.abspath(os.path.dirname(__file__))
+PLUGIN_NAME = 'Php Tidy'
+
+if 3 == PY_VERSION:
+    # python3 and st3
+    from . import shutilwhich
+else:
+    # python2 and st2
+    import shutilwhich
+
+
+class PhpTidyCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        view = self.view
+
+        # ensure php in path
+        php_path = None
+        php_path = shutil.which('php')
+        if php_path is None:
+            sublime.error_message(
+                '%s Error\n\n'
+                'PHP must be installed and added to your\n'
+                'environment PATH variable in order to use\n'
+                'this plug-in.' % (PLUGIN_NAME))
+            return
+
+        # esnure php tidy script (allman.php)
+        self.tidy_path = os.path.join(PLUGIN_PATH, 'allman.php')
+        if not os.path.exists(self.tidy_path):
+            sublime.error_message(
+                '%s Error\n\n'
+                'Tidy script missing.\n\n'
+                '%s' % (PLUGIN_NAME, self.tidy_path))
+            return
+
+        # remember the current viewport position
+        cur_point = view.sel()[0].a
+        y = view.text_to_layout(cur_point)[1]
+        x = view.viewport_position()[0]
+
+        # configure paths
+        self.temp_file_path = self._save_view_in_temp_file(view)
+        self.temp_file_name = os.path.basename(self.temp_file_path)
+        self.temp_dir = os.path.dirname(os.path.realpath(self.temp_file_path))
+        self.tidy_bkup_file_path = os.path.normpath(os.path.join(self.temp_dir, '.' + self.temp_file_name + '.phptidybak~'))
+        self.tidy_cache_file_path = os.path.join(self.temp_dir, '.phptidy-cache')
+
+        startupinfo = None
+        if OS_PLATFORM == 'Windows':
+            # prevents command prompt from displaying on windows
+            startupinfo = subprocess.STARTUPINFO()
+            startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+            startupinfo.wShowWindow = subprocess.SW_HIDE
+
+        # run php tidy on the tmp file
+        proc = subprocess.Popen([
+            php_path, self.tidy_path, 'replace', self.temp_file_path],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            startupinfo=startupinfo,
+            cwd=self.temp_dir)
+
+        # any output from process indicates an error occurred
+        stdout = proc.communicate(input=None)[0]
+        if stdout:
+            sublime.error_message(
+                '%s Error\n\n'
+                '%s' % (PLUGIN_NAME, stdout.decode('utf-8')))
+            self._clean_up()
+            return
+
+        # ensure tidy content file
+        if not os.path.exists(self.temp_file_path):
+            sublime.error_message(
+                '%s Error\n\n'
+                'Fixed PHP temp file missing.\n\n'
+                '%s' % (PLUGIN_NAME, self.temp_file_path))
+            self._clean_up()
+            return
+
+        # read tidy content file
+        fh = open(self.temp_file_path, 'rb')
+        fixed = fh.read().decode('utf-8')
+        fh.close()
+
+        # normalize line endings (unix/lf)
+        fixed = fixed.replace('\r\n', '\n').replace('\r', '\n')
+
+        self._clean_up()
+
+        # check for changes in the source and update view
+        if fixed != self.source:
+            view.replace(edit, self.replace_region, fixed)
+            # scroll viewport to the top of previous position
+            sublime.set_timeout(lambda: view.set_viewport_position((x, y - 1.0 * view.line_height())), 0)
+            # update status bar
+            sublime.set_timeout(lambda: sublime.status_message(PLUGIN_NAME + ': code fixed.'), 0)
+        else:
+            # source text same as fixed text...
+            sublime.set_timeout(lambda: sublime.status_message(PLUGIN_NAME + ': nothing to fix.'), 0)
+
+    def _save_view_in_temp_file(self, view):
+        # read buffer contents
+        self.replace_region = sublime.Region(0, view.size())
+        self.source = view.substr(self.replace_region)
+
+        # create the temp file
+        tf = tempfile.NamedTemporaryFile(mode='wb', suffix='.php', delete=False)
+        tf.write(self.source.encode('utf-8'))
+        tf.close()
+
+        # returns the full path to the temp file
+        return tf.name
+
+    def _clean_up(self):
+        # delete tmp files
+        if os.path.exists(self.temp_file_path):
+            os.remove(self.temp_file_path)
+        if os.path.exists(self.tidy_bkup_file_path):
+            os.remove(self.tidy_bkup_file_path)
+        if os.path.exists(self.tidy_cache_file_path):
+            os.remove(self.tidy_cache_file_path)
+
+    def is_enabled(self):
+        return True if re.search(
+            'php', self.view.settings().get('syntax'), re.IGNORECASE) else False
